a0bd3a318376790f30906d2518f0be51bfb3134a
rkodev <43806892+rkodev@users.noreply.github.com>
2022-08-31 19:23:41 +0300

    Pass a Context parameter to accesstoken and base bearer token provider

--- authentication/access_token_provider.go
+++ authentication/access_token_provider.go
@@ -1,13 +1,14 @@
 package authentication
 
 import (
+	"context"
 	u "net/url"
 )
 
 //AccessTokenProvider returns access tokens.
 type AccessTokenProvider interface {
 	// GetAuthorizationToken returns the access token for the provided url.
-	GetAuthorizationToken(url *u.URL, additionalAuthenticationContext map[string]interface{}) (string, error)
+	GetAuthorizationToken(context context.Context, url *u.URL, additionalAuthenticationContext map[string]interface{}) (string, error)
 	// GetAllowedHostsValidator returns the hosts validator.
 	GetAllowedHostsValidator() *AllowedHostsValidator
 }

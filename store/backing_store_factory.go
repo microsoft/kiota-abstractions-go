cd8d6e03d98b355f7cbd154ec9ba020e32dd11a2
rkodev <43806892+rkodev@users.noreply.github.com>
2023-02-28 10:20:09 +0300

    Fix backingstore type

--- store/backing_store_factory.go
+++ store/backing_store_factory.go
@@ -6,4 +6,4 @@ type BackingStoreFactory func() BackingStore
 
 // BackingStoreFactoryInstance returns a backing store instance.
 // if none exists an instance of InMemoryBackingStore is initialized and returned
-var BackingStoreFactoryInstance = NewInMemoryBackingStore
+var BackingStoreFactoryInstance BackingStoreFactory = NewInMemoryBackingStore

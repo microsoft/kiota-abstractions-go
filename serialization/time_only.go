6c436c9ee5f35fc264179e2ee2c94db1a9db580c
Musale Martin <martinmusale@microsoft.com>
2025-05-05 14:38:38 +0300

    Add a precision property to hold the precision for a TimeOnly time

--- serialization/time_only.go
+++ serialization/time_only.go
@@ -1,14 +1,15 @@
 package serialization
 
 import (
-	"errors"
+	"fmt"
 	"strings"
 	"time"
 )
 
 // TimeOnly is represents the time part of a date time (time) value.
 type TimeOnly struct {
-	time time.Time
+	time      time.Time
+	precision int // number of decimal places for nanoseconds
 }
 
 const timeOnlyFormat = "15:04:05.000000000"
@@ -28,35 +29,44 @@ var timeOnlyParsingFormats = map[int]string{
 
 // String returns the time only as a string following the RFC3339 standard.
 func (t TimeOnly) String() string {
-	return t.time.Format(timeOnlyFormat)
+	if t.precision == 0 {
+		return t.time.Format("15:04:05")
+	}
+	return t.time.Format(timeOnlyParsingFormats[t.precision])
 }
 
 // ParseTimeOnly parses a string into a TimeOnly following the RFC3339 standard.
 func ParseTimeOnly(s string) (*TimeOnly, error) {
-	if len(strings.TrimSpace(s)) <= 0 {
+	s = strings.TrimSpace(s)
+	if s == "" {
 		return nil, nil
 	}
-	splat := strings.Split(s, ".")
-	parsingFormat := timeOnlyParsingFormats[0]
-	if len(splat) > 1 {
-		dotSectionLen := len(splat[1])
-		if dotSectionLen >= len(timeOnlyParsingFormats) {
-			return nil, errors.New("too many decimal places in time only string")
+
+	precision := 0
+	if parts := strings.Split(s, "."); len(parts) > 1 {
+		precision = len(parts[1])
+		if precision >= len(timeOnlyParsingFormats) {
+			return nil, fmt.Errorf("time precision of %d exceeds maximum allowed of %d", precision, len(timeOnlyParsingFormats)-1)
 		}
-		parsingFormat = timeOnlyParsingFormats[dotSectionLen]
 	}
-	timeValue, err := time.Parse(parsingFormat, s)
+
+	timeValue, err := time.Parse(timeOnlyParsingFormats[precision], s)
 	if err != nil {
 		return nil, err
 	}
-	return &TimeOnly{
-		time: timeValue,
-	}, nil
+
+	return &TimeOnly{time: timeValue, precision: precision}, nil
 }
 
 // NewTimeOnly creates a new TimeOnly from a time.Time.
 func NewTimeOnly(t time.Time) *TimeOnly {
+	precision := 0
+	nanos := t.Nanosecond()
+	if nanos > 0 {
+		precision = len(strings.TrimRight(fmt.Sprintf("%09d", nanos), "0"))
+	}
 	return &TimeOnly{
-		time: t,
+		time:      t,
+		precision: precision,
 	}
 }
